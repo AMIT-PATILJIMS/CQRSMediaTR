d13365be00e8e97789cdccd31dd3d44152b147ef
AMIT-PATILJIMS <ap1079997@gmail.com>
2023-05-03 19:48:46 +0530

    Changes: 1. Implement Command and Queries.

---
+++ Queries/GetStudentByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using CQRSMediaTR.Models;
+using MediatR;
+
+namespace CQRSMediaTR.Queries
+{
+    public class GetStudentByIdQuery : IRequest<StudentDetails>
+    {
+        public int Id { get; set; }
+    }
+}
