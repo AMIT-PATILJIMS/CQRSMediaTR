9f3895be8c181faca8de18311505711d7178d1d8
AMIT-PATILJIMS <ap1079997@gmail.com>
2023-05-03 09:49:08 +0530

    Changes: 1. Add New folders in Project. 2. Add new model StudentDeatils.cs.

---
+++ Models/StudentDetails.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace CQRSMediaTR.Models
+{
+    public class StudentDetails
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentEmail { get; set; }
+        public string StudentAddress { get; set; }
+        public int StudentAge { get; set; }
+    }
+}
