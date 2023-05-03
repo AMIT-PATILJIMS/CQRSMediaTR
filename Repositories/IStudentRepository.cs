30ad3bc83d6b9cb3f02be86248a4dca6c0666d89
AMIT-PATILJIMS <ap1079997@gmail.com>
2023-05-03 18:39:15 +0530

    Changes: 1. Add interfaces and class in Repository. 2. Implement Repository pattern.

---
+++ Repositories/IStudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using CQRSMediaTR.Models;
+
+namespace CQRSMediaTR.Repositories
+{
+    public interface IStudentRepository
+    {
+        public Task<List<StudentDetails>> GetStudentListAsync();
+
+        public Task<StudentDetails> GetStudentByIdAsync(int Id);
+
+        public Task<StudentDetails> AddStudentAsync(StudentDetails studentDetails);
+
+        public Task<int> UpdateStudentAsync(StudentDetails studentDetails);
+
+        public Task<int> DeleteStudentAsync(int Id);
+    }
+}
